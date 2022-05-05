5986231af207c8896e33f5f196844482a6593ae7
Kingofbeer123 <kiril.shevchenko123@gmail.com>
2022-05-05 12:22:06 +0300

    test

--- src/index.js
+++ src/index.js
@@ -5,44 +5,56 @@ import { goods } from './goods.js';
  * @returns {*}
  */
 const brandFilter = (brand) => {
-  return goods.filter((product) => product.brand === brand);
-};
+   goods.filter(item => item.brand === brand)
+}
 
 /**
  * @param {string} color
  * @returns {*}
  */
-const colorFilter = (color) => {};
+const colorFilter = (color) => {
+  goods.filter(item => item.color === color)
+};
 
 /**
  * @param {string} model
  * @returns {*}
  */
-const modelFilter = (model) => {};
+const modelFilter = (model) => {
+  goods.filter(item => item.model === model)
+};
 
 /**
  * @param {number} memory
  * @returns {*}
  */
-const memoryFilter = (memory) => {};
+const memoryFilter = (memory) => {
+  goods.filter(item => item.memory === memory)
+};
 
 /**
  * @param {number} price
  * @returns {*}
  */
-const priceFilter = (price) => {};
+const priceFilter = (price) => {
+  goods.filter(item => item.price === price)
+};
 
 /**
  * @param {string} country
  * @returns {*}
  */
-const countryFilter = (country) => {};
+const countryFilter = (country) => {
+  goods.filter(item => item.country === country)
+};
 
 /**
  * @param {string} os
  * @returns {*}
  */
-const osFilter = (os) => {};
+const osFilter = (os) => {
+  goods.filter(item => item.os === os)
+};
 
 /**
  * @param {number} from
